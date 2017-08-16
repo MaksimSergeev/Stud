c4e09a4bd1d88e21579ed82e4c2f215d7909b525
MaksimSergeev <hydroid89@gmail.com>
2017-08-16 18:51:31 +0300

    +

---
+++ MVC/models/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+class Article
+{
+
+    public static function getArticleItemById($id)
+    {
+        //require to DB
+    }
+
+    public static function getArticleList()
+    {
+        //require to DB
+    }
+}
