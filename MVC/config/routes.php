404c4019c24e13d12197e11d42ecb39b5381eec0
Maksim Sergeev <hydroid89@gmail.com>
2017-08-16 18:57:40 +0300

    17

--- MVC/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-
-    'news/([0-9]+)' => 'news/view/$1',
-
-    'news' => 'news/index', // call actionIndex in NewsController
-    );
--- config/db_params.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array (
-    'host' => 'mvc',
-    'dbname' => 'first',
-    'user' => 'root',
-    'password' => '',
-);
